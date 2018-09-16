a4fcf2bfdc2cadb46c92cbf3ad59b08fb4d8c9c8
Wolfy-J <wolfy.jd@gmail.com>
2018-09-16 19:56:54 +0300

    CS

---
+++ tests/Filters/BaseTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Filters\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+
+}
